c495d3dddd89e406dcbee924c7e6ddd6fb0909c6
Harsh <harrsh0502@gmail.com>
2025-11-19 19:26:32 +0530

    decimal

---
+++ DataTypes/chapter_4.py
@@ -0,0 +1,18 @@
+is_boiling = True
+string_count = 5
+total_actions = string_count + is_boiling #upcasting
+print(f"Total actions: {total_actions}")
+
+milk_present = 0 # no milk
+print(f"Is there milk? {bool(milk_present)}")
+
+water_hot = True
+tea_added = True
+
+can_sever = water_hot and tea_added
+print(f"Can serve chai? {can_sever}")
+
+
+
+
+
