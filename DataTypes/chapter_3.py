2c081b59a33855a23ada72705ab1b831d321b4a7
Harsh <harrsh0502@gmail.com>
2025-11-19 18:42:30 +0530

    ai

--- DataTypes/chapter_3.py
+++ DataTypes/chapter_3.py
@@ -1,20 +1,30 @@
 # integer
 
+
+# Addition ( + )
 black_tea_grams = 14
 ginger_grams = 3
-
 total_grams = black_tea_grams + ginger_grams
 print(f"Total grams of base tea is {total_grams}")
 
+# Substraction ( - )
 remaining_tea = black_tea_grams - ginger_grams
 print(f"Total grams of remaining tea is {remaining_tea}")
 
+# Divide ( / )
 milk_litres = 7
 servings = 4
 milk_per_serving = milk_litres / servings
 print(f"Milk per serving: {milk_per_serving}")
 
+# Divide ( // ) but it shows with decimal output
 total_tea_bags = 7
 pots = 4
 bags_per_pot = total_tea_bags // pots
 print(f"While tea bags per pot: {bags_per_pot}")
+
+# Modulus ( % ) gives remainder
+total_cardamom_pots = 10
+pots_per_cup = 3
+leftover_pots = total_cardamom_pots % pots_per_cup
+print(f"Leftover C pots: {leftover_pots}")
