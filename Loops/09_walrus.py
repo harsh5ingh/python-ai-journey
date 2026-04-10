94324b631b1a72bf6114a9958686cf5783b98d65
Harsh <harrsh0502@gmail.com>
2026-04-10 19:30:48 +0530

    walrus

--- Loops/09_walrus.py
+++ Loops/09_walrus.py
@@ -6,5 +6,22 @@
 
 value = 13
 
-if (remainder := value % 5):
+if (remainder := value % 5):             # Walrus ( := ) 
   print(f"Not Divisible, remainder is {remainder}")
+
+available_sizes = ["small", "medium", "large"]
+
+if (requested_size := input("Enter your chai cup size: ")) in available_sizes:
+  print(f"Serving {requested_size} chai")
+else:
+  print(f"Size is unavailable = {requested_size}")
+
+
+flavors = ["masala", "ginger", "lemon", "mint"]
+
+print("Available flavors: ", flavors)
+
+while (flavor := input("Choose your flavour: ")) not in flavors:
+  print(f"Sorry, {flavor} is not available, right now")
+
+print(f"You choose {flavor} chai")
