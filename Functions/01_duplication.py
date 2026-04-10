94cbe7863b54379267645b5514473fee9a8dccbf
Harsh <harrsh0502@gmail.com>
2026-04-11 03:00:12 +0530

    Functions

---
+++ Functions/01_duplication.py
@@ -0,0 +1,10 @@
+def print_order(name, chai_type):
+  print(f"{name} ordered {chai_type} chai!")
+
+
+
+print_order("Aman", "Masala")
+print_order("Hitesh", "Ginger")
+print_order("Jia", "Tulsi")
+print_order("Harsh", "Tea leaf only")
+
