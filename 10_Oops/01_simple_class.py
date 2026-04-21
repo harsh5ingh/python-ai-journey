55a758124432b785b7e05633e28ba35218c23b81
Harsh <harrsh0502@gmail.com>
2026-04-22 00:25:00 +0530

    oops - class & objects

---
+++ 10_Oops/01_simple_class.py
@@ -0,0 +1,12 @@
+class Chai:
+  pass
+
+class ChaiTime:
+  pass
+
+print(type(Chai))
+
+ginger_Tea = Chai()
+print(type(ginger_Tea))
+print(type(ginger_Tea) is Chai)
+print(type(ginger_Tea) is ChaiTime)
