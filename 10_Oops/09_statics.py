f5eb2cb8172eee8accb541b86291146e82594e8d
Harsh <harrsh0502@gmail.com>
2026-04-27 00:03:12 +0530

    statics

--- 10_Oops/09_statics.py
+++ 10_Oops/09_statics.py
@@ -1,3 +1,17 @@
 class ChaiUtils:
 
-  def
+  @staticmethod
+  def clean_ingredients(text):
+    return [item.strip() for item in text.split(",")]
+  
+#"water, milk   , ginger,  "
+
+raw = "water, milk, ginger, honey"
+
+#obj = ChaiUtils()
+#obj.clean_ingredients(raw)
+
+
+
+cleaned = ChaiUtils.clean_ingredients(raw)
+print(cleaned)
